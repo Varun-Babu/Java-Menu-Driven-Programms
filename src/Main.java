af85dce9255d41ead7d6548dc73e9ef568e87b34
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-21 12:27:15 +0530

    largeornot added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
+    public static int largeOrNot(int i,int j, int k){
+        int a = Math.max(i,j);
+        int b = Math.max(a,k);
+        return b;
+   }
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+
     }
 }
