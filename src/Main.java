f35923b44a5e3f007f283f29cc7b3e3e394a9483
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-21 12:31:13 +0530

    small added

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,15 @@ public class Main {
         int b = Math.max(a,k);
         return b;
    }
+    public static int smallOrNot(int i,int j, int k){
+        int a = Math.min(i,j);
+        int b = Math.min(a,k);
+        return b;
+    }
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
 
 
     }
